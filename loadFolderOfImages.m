501dc8c4168b4d80ce9aaae71b0bc332f83b12de
Anish <aks5db@virginia.edu>
2014-03-17 23:26:21 -0400

    fixed some bugs, created more

--- loadFolderOfImages.m
+++ loadFolderOfImages.m
@@ -1,6 +1,6 @@
 %% Load folder of images
 dbstop if error
-numOfImages = 31;
+numOfImages = 250;
 folder_path = 'C:\Users\anish\Documents\MATLAB\002C\';
 file_base = 'HUNT';
 file_ext = '.JPG';
@@ -8,21 +8,21 @@ file_path = strcat(folder_path, file_base);
 height = 1080; width = 1920;
 vidFrames = zeros(height, width, numOfImages, 'uint8');
 
-rgbArray = zeros(3, numOfImages);
-hsvArray = zeros(1, numOfImages);
+%rgbArray = zeros(3, numOfImages);
+%hsvArray = zeros(1, numOfImages);
 
 for n=1:1:numOfImages
     file_name = strcat(file_path, sprintf('%04d',n));
     file_name = strcat(file_name, file_ext);
     
     rgbImg = imread(file_name);
-    hsvImg = rgb2hsv(rgbImg);
+    %hsvImg = rgb2hsv(rgbImg);
     
-    rgbArray(1, n) = mean(mean(rgbImg(:, :, 1)));
-    rgbArray(2, n) = mean(mean(rgbImg(:, :, 2)));
-    rgbArray(3, n) = mean(mean(rgbImg(:, :, 3)));
+    %rgbArray(1, n) = mean(mean(rgbImg(:, :, 1)));
+    %rgbArray(2, n) = mean(mean(rgbImg(:, :, 2)));
+    %rgbArray(3, n) = mean(mean(rgbImg(:, :, 3)));
     
-    hsvArray(1, n) = mean(mean(hsvImg(:, :, 1))); 
+    %hsvArray(1, n) = mean(mean(hsvImg(:, :, 1))); 
     
     
     
@@ -30,20 +30,20 @@ for n=1:1:numOfImages
 end
 
 %% Plot Data 
-plot(rgbArray(2, :)')
-title('Green Intensities over time in RGB Space')
-xlabel('Frame Number')
-ylabel('Color Intensity from 0 to 255')
-hold on; 
-greenMean = mean(rgbArray(2, :)); 
-x=[0 numOfImages];
-y=[greenMean greenMean];
-plot(x, y, 'r')
-
-[idx ctrs] = kmeans(rgbArray(2, :), 2);
-figure; plot(idx)
-ylim([0.8 2.2])
-title('kmeans result')
+% plot(rgbArray(2, :)')
+% title('Green Intensities over time in RGB Space')
+% xlabel('Frame Number')
+% ylabel('Color Intensity from 0 to 255')
+% hold on; 
+% greenMean = mean(rgbArray(2, :)); 
+% x=[0 numOfImages];
+% y=[greenMean greenMean];
+% plot(x, y, 'r')
+% 
+% [idx ctrs] = kmeans(rgbArray(2, :), 2);
+% figure; plot(idx)
+% ylim([0.8 2.2])
+% title('kmeans result')
 
 %% Crop Video
 startCol = 300+180;
--- sifttracker.m
+++ sifttracker.m
@@ -2,34 +2,50 @@
 
 dbstop if error; close all;
 
+%Initialize based on user inputted values 
 initialImg = single(croppedFrames(:, :, 1));
 
-startRow = 700; endRow = 780;
-startCol = 500; endCol = 570;
+%startRow = 700; endRow = 780;
+%startCol = 500; endCol = 570;
+startRow = 380; endRow = 480;
+startCol = 500; endCol = 800;
+
 template1 = initialImg(startRow:endRow, startCol:endCol);
+
+averageRowDifference = zeros(size(croppedFrames, 3), 1); 
+averageColDifference = zeros(size(croppedFrames, 3), 1); 
 
 for n=2:1:(size(croppedFrames, 3)-1)
 fprintf('Current Frame: %d \n', n); 
 
+% Calculate SIFTs and matches 
 nextFrame = single(croppedFrames(:, :, n));
 
-[ftem, da] = vl_sift(template1) ;
+[ftemplate, da] = vl_sift(template1) ;
 [fb, db] = vl_sift(nextFrame) ;
 
 [matches, scores] = vl_ubcmatch(da, db) ;
-plotMatches(matches, template1, nextFrame, ftem, fb);
+plotMatches(matches, template1, nextFrame, ftemplate, fb);
 title(strcat('Frame Number : ', num2str(n))); 
 
+% Save output as an image 
 str = strcat('figfiles/', 'frame_', num2str(n)); 
 saveas(gcf,strcat(str,'.png'));
 
+% Aggreate matches and eliminate outliers 
 xVals = zeros(numel(matches(1,:)), 1);
 yVals = zeros(numel(matches(1,:)), 1);
+xTVals = zeros(numel(matches(1,:)), 1);
+yTVals = zeros(numel(matches(1,:)), 1);
 
 for k=1:numel(matches(1,:))
-    
+    % X,Y values for the result of matching on the new Frame
     xVals(k) = fb(1, matches(2, k));
     yVals(k) = fb(2, matches(2, k));
+    
+    % Original X, Y values for the matched points on the Template
+    xTVals(k) = ftemplate(1, matches(1, k)); 
+    yTVals(k) = ftemplate(2, matches(1, k)); 
     
 end
 
@@ -39,8 +55,22 @@ outlier_idx = abs(xVals - median(xVals)) > 1*std(xVals) | ...
 xVals(outlier_idx) = []; 
 yVals(outlier_idx) = []; 
 
-%Create new template 
+%Determine average shift 
+rowShift = zeros(length(xVals), 1); 
+colShift = zeros(length(xVals), 1); 
 
+for k=1:1:length(xVals)
+    translatedTemplateRow = startRow + yTVals(k); 
+    translatedTemplateCol = startCol + xTVals(k); 
+    
+    rowShift(k) = yVals(k) - translatedTemplateRow; 
+    colShift(k) = xVals(k) - translatedTemplateCol; 
+end 
+
+averageRowDifference(n) = mean(rowShift); 
+averageColDifference(n) = mean(colShift); 
+
+%Create new template 
 fudgeRow = 90; fudgeCol = 100; 
 
 if ((max(yVals) - min(yVals)) < 90)
@@ -54,7 +84,5 @@ if ((max(xVals) - min(xVals)) < 100)
 end 
 
 
-%startRow = 700; endRow = 780;
-%startCol = 500; endCol = 570;
 template1 = nextFrame(startRow:endRow, startCol:endCol);
 end 
